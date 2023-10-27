f898f612f484b8abff353f3d7b30790ef5028bf5
ndresgarc <andres.garcia.cavero@nttdata.com>
2023-10-27 23:16:35 +0200

    feat: remove react app

--- react/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders logo', () => {
-  render(<App />);
-  const logoText = screen.getByText(/NDRESGARC/i);
-  expect(logoText).toBeInTheDocument();
-});
